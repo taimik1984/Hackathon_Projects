cfd1b467845d150e5e0f50d3178c809708fe60d5
taimik1984 <104464564+taimik1984@users.noreply.github.com>
2022-04-26 22:56:43 +0200

    Update twitter_credentials.py

--- COVID19_Twitter_Project/Tutorial1/twitter_credentials.py
+++ COVID19_Twitter_Project/Tutorial1/twitter_credentials.py
@@ -2,7 +2,7 @@
 # Variables which contain the user credentials needed to access the Twitter API
 # Copy and paste your credentials within the quotation marks '' as these are string variables
 
-api_key = ''
-api_secret_key =  ''
-access_token = ''
-access_token_secret = ''
+api_key = 'C0G53nWrn86aus99tSvPgPIZW'
+api_secret_key =  'Yng0ml3hrXKo5ttbkAv02vQA0tXFcq7CmAIDHwY1i1AnzLs608'
+access_token = '913683802278555649'
+access_token_secret = '913683802278555649-NMupD3qCQkZFm3ApShyOtDtbNbmKny5'
